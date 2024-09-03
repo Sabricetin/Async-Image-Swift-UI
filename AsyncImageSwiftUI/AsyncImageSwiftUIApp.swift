bc56c1972ab8a6f6b351f803121474195a603c84
Sabricetin <sabriyazilim@gmail.com>
2024-09-03 17:17:04 +0300

    Initial Commit

---
+++ AsyncImageSwiftUI/AsyncImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageSwiftUIApp.swift
+//  AsyncImageSwiftUI
+//
+//  Created by Sabri Çetin on 3.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
